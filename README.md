639f16fe0411c99dbfdd32acecf1e7e58ff48a78
Arshad Chowdhury <arshadchowdhury46@gmail.com>
2022-02-15 20:04:28 +0600

    Daily Blog Updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Daily-Blog
+## This a project to upload daily blog of anyone from the internet.
+The app is ready at https://shielded-tundra-03733.herokuapp.com/ <br>
+It takes the post and put it in the Database using MongoDB and render it back instantly. <br>
+Then it shortens the whole text in 100 words or so and add a read more tag, when read more is clicked it creates a separate page and render the whole thing.
---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const homeStartingContent = "Welcome Everyone, ";
+const aboutContent =
+  "I am Arshad Web Desinger & Developer. I'm striving for excellence in this field. I'm trying to learn more and more everyday and be very good at web development someday.";
+const contactContent =
+  "You can call me or Whatsapp me on +8801676830816 if you wanna contact";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect(
+  "mongodb+srv://Admin-Arshad:ArshaD007@cluster0.spd21.mongodb.net/blogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
